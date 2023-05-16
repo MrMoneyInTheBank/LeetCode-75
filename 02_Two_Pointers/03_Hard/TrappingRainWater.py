58c4a95743767cfaf2548e64185bd4bb18d7376c
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2023-05-16 15:10:51 -0400

    Two pointers

--- 02_Two_Pointers/03_Hard/TrappingRainWater.py
+++ 02_Two_Pointers/03_Hard/TrappingRainWater.py
@@ -1,3 +1,9 @@
+"""
+Given n non-negative integers representing an elevation map 
+where the width of each bar is 1, compute how much water it can 
+trap after raining.
+"""
+
 def main(height):
     l, r = 0, len(height) - 1 
     leftmax, rightmax = height[l], height[r]
@@ -15,5 +21,4 @@ def main(height):
         
     return res
 
-
-print(main([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+# Time | Space : O(n) | O(1)
