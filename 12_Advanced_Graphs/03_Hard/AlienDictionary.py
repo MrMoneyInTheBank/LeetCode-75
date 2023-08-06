35f9b8b1f9699a4390a6818c8395139732f7fcea
Anshuman Singh <109028367+MrMoneyInTheBank@users.noreply.github.com>
2023-08-05 22:22:45 -0500

    Alien Dictionary Fixed

--- 12_Advanced_Graphs/03_Hard/AlienDictionary.py
+++ 12_Advanced_Graphs/03_Hard/AlienDictionary.py
@@ -3,38 +3,35 @@ from typing import List
 class Solution:
 	def alienOrder(self, words: List[str]) -> str:
 		graph = {char: set() for word in words for char in word}
-
+                
 		for i in range(len(words) - 1):
 			word1, word2 = words[i], words[i+1]
 			shorter = min(len(word1), len(word2))
 
-			if len(word1) > len(word2) and word1[:shorter] == w2[:shorter]:
+			if len(word1) > len(word2) and word1[:shorter] == word2[:shorter]:
 				return ""
-
+			
 			for j in range(shorter):
 				if word1[j] != word2[j]:
-					graph[word1[j], word2[j]]
+					graph[word1[j]].add(word2[j])
 					break
-
-		seen = {}
+			
 		res = []
+		seen = {}
 
 		def dfs(char):
 			if char in seen:
 				return seen[char]
-
-			visited[char] = True
-
+		
+			seen[char] = True
 			for child in graph[char]:
 				if dfs(child):
 					return True
-
-			visited[char] = False
+			seen[char] = False 
 			res.append(char)
-
+		
 		for char in graph:
-			if dfs(char): # res has already been reversed
+			if dfs(char):
 				return ""
-
-		res.reverse()
-		return "".join(res)
+		
+		return "".join(res[::-1])
